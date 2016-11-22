5a51b40ef6f5a3d0f39b1f68165fce993ad1a754
kevin <kevin@DESKTOP-2BJLHLB>
2016-11-22 20:46:45 +0100

    Added analogue clock feature in start-class

--- src/rotatingline/Start.java
+++ src/rotatingline/Start.java
@@ -37,15 +37,31 @@ public class Start
 //    RotatingLine rL = new RotatingLine(50);
 //    kiste.add(rL);
     
-    translateLine tL = new translateLine(800);
-    kiste.add(tL);
+    translateLine[] lines = new translateLine[3];
+
+      lines[0] = new translateLine(1000);
+      lines[0].setOpaque(false);
+      kiste.add(lines[0]);
+      lines[1] = new translateLine(60000);
+      lines[1].setOpaque(false);
+      kiste.add(lines[1]);
+      lines[2] = new translateLine(3600000);
+      lines[2].setOpaque(false);
+      kiste.add(lines[2]);
+//    translateLine tL = new translateLine(800);
+//    kiste.add(tL);
     
+      for(int i=0;i<lines.length;i++)
+      {
+        lines[i].start();
+      }
+
     frm.setSize(300,300);
     frm.setLocationRelativeTo(null);
     frm.setTitle("Rotating Line");
     frm.setVisible(true);
     
-    tL.start();
+//    tL.start();
 //    rL.start();
   }
 }
